c88af67b2d870281e16d601d4280547dc7f01d80
Håkon Vågsether <haakonsv@gmail.com>
2020-01-14 00:52:36 +0000

    Separated more code

--- python/updated_geometric_planner.py
+++ python/updated_geometric_planner.py
@@ -1,4 +1,5 @@
 import math
+import sys
 
 from ompl import util as ou
 from ompl import base as ob
@@ -6,10 +7,9 @@ from ompl import geometric as og
 from ompl import control as oc
 from math import sqrt
 
-import sys
+import planner_helpers as ph
 
-import numpy as np
-import matplotlib.pyplot as plt
+from plotting import plot_path
 
 
 class Obstacle:
@@ -19,123 +19,9 @@ class Obstacle:
         self.radius = radius
 
 
-class ValidityChecker(ob.StateValidityChecker):
-    def __init__(self, si, obstacles):
-        super(ValidityChecker, self).__init__(si)
-        self.obstacles = obstacles
-
-    # Returns whether the given state's position overlaps the
-    # circular obstacle
-    def isValid(self, state):
-        x = state.getX()
-        y = state.getY()
-        for obstacle in self.obstacles:
-            if sqrt(pow(x - obstacle.x, 2) + pow(y - obstacle.y, 2)) - obstacle.radius <= 0:
-                return False
-
-        return True
-
-    # Returns the distance from the given state's position to the
-    # boundary of the circular obstacle.
-    def clearance(self, state):
-        if len(self.obstacles) == 0:
-            return 1
-
-        # Extract the robot's (x,y) position from its state
-        x = state.getX()
-        y = state.getY()
-
-        clearance = 0
-        # Distance formula between two points, offset by the circle's
-        # radius
-        for obstacle in self.obstacles:
-
-            clearance += (sqrt(pow(x - obstacle.x, 2) + pow(y - obstacle.y, 2)) - obstacle.radius)
-            if clearance <= 0:
-                return 0
-
-        return clearance
-
-
 def absolute_distance_between_angles(angle1, angle2):
     fabs = math.fabs(math.atan2(math.sin(angle1 - angle2), math.cos(angle1 - angle2)))
     return fabs
-
-
-def get_path_length_objective(si):
-    return ob.PathLengthOptimizationObjective(si)
-
-
-def get_threshold_path_length_objective(si):
-    obj = ob.PathLengthOptimizationObjective(si)
-    # obj.setCostThreshold(ob.Cost(8))
-    return obj
-
-
-class ClearanceObjective(ob.StateCostIntegralObjective):
-    def __init__(self, si):
-        super(ClearanceObjective, self).__init__(si, True)
-        self.si_ = si
-
-    # Our requirement is to maximize path clearance from obstacles,
-    # minimization. Therefore, we set each state's cost to be the
-    # reciprocal of its clearance, so that as state clearance
-    # increases, the state cost decreases.
-    def stateCost(self, s):
-        if (self.si_.getStateValidityChecker().clearance(s) == 0):
-            return sys.maxsize
-        return ob.Cost(1 / (self.si_.getStateValidityChecker().clearance(s))**0.5)
-
-class MinTurningObjective(ob.StateCostIntegralObjective):
-    def __init__(self, si):
-        super(MinTurningObjective, self).__init__(si, True)
-        self.si_ = si
-
-    # This objective function punishes the boat for tacking or jibing
-    # by adding a 500 cost for turns over 60 degrees
-    def motionCost(self, s1, s2):
-        direction = math.atan2(s2.getY() - s1.getY(), s2.getX() - s1.getX())
-        try:
-            diff = math.fabs(direction - self.last_direction)
-        except AttributeError:
-            self.last_direction = direction
-            diff = math.fabs(direction - self.last_direction)
-        if diff > math.radians(60):
-            return 500.0
-        else:
-            return diff*10
-
-class WindObjective(ob.StateCostIntegralObjective):
-    def __init__(self, si):
-        super(WindObjective, self).__init__(si, True)
-        self.si_ = si
-
-    # This objective function punishes the boat for going up/downwind
-    def motionCost(self, s1, s2):
-        direction = math.atan2(s2.getY() - s1.getY(), s2.getX() - s1.getX())
-        distance = ((s2.getY() - s1.getY())**2 + (s2.getX() - s1.getX())**2)**0.5
-        wind_direction = math.radians(90)
-        relative_wind_direction = wind_direction - direction
-
-        upwind_angle = math.radians(30)
-        downwind_angle = math.radians(30)
-
-        if math.fabs(relative_wind_direction) < upwind_angle:
-            multiplier = sys.maxsize
-        elif math.fabs(relative_wind_direction - math.radians(180)) < downwind_angle:
-            multiplier = sys.maxsize
-        else:
-            multiplier = 0.0
-
-        return multiplier * distance
-
-def get_clearance_objective(si):
-    return ClearanceObjective(si)
-
-def get_path_length_obj_with_cost_to_go(si):
-    obj = ob.PathLengthOptimizationObjective(si)
-    obj.setCostToGoHeuristic(ob.CostToGoHeuristic(ob.goalRegionCostToGo))
-    return obj
 
 
 def allocatePlanner(si, plannerType):
@@ -155,64 +41,6 @@ def allocatePlanner(si, plannerType):
         return og.SORRTstar(si)
     else:
         ou.OMPL_ERROR("Planner-type is not implemented in allocation function.")
-
-# Keep these in alphabetical order and all lower case
-def allocate_planner(si, planner_type, decomp):
-    if planner_type.lower() == "est":
-        return og.EST(si)
-    elif planner_type.lower() == "kpiece":
-        return og.KPIECE1(si)
-    elif planner_type.lower() == "pdst":
-        return og.PDST(si)
-    elif planner_type.lower() == "rrt":
-        return og.RRT(si)
-    elif planner_type.lower() == "syclopest":
-        return og.SyclopEST(si, decomp)
-    elif planner_type.lower() == "sycloprrt":
-        return og.SyclopRRT(si, decomp)
-
-    else:
-        ou.OMPL_ERROR("Planner-type is not implemented in allocation function.")
-
-def getBalancedObjective(si):
-    lengthObj = ob.PathLengthOptimizationObjective(si)
-    clearObj = ClearanceObjective(si)
-    minTurnObj = MinTurningObjective(si)
-    windObj = WindObjective(si)
-
-    opt = ob.MultiOptimizationObjective(si)
-    opt.addObjective(lengthObj, 1.0)
-    opt.addObjective(clearObj, 1.0)
-    opt.addObjective(minTurnObj, 1.0)
-    opt.addObjective(windObj, 1.0)
-    # opt.setCostThreshold(ob.Cost(5))
-
-    return opt
-
-# Keep these in alphabetical order and all lower case
-def allocate_objective(si, objectiveType):
-    if objectiveType.lower() == "pathclearance":
-        return get_clearance_objective(si)
-    elif objectiveType.lower() == "pathlength":
-        return get_path_length_objective(si)
-    elif objectiveType.lower() == "thresholdpathlength":
-        return get_threshold_path_length_objective(si)
-    elif objectiveType.lower() == "weightedlengthandclearancecombo":
-        return getBalancedObjective(si)
-    else:
-        ou.OMPL_ERROR("Optimization-objective is not implemented in allocation function.")
-
-
-def create_numpy_path(states):
-    lines = states.splitlines()
-    length = len(lines) - 1
-    array = np.zeros((length, 2))
-
-    for i in range(length):
-        array[i][0] = float(lines[i].split(" ")[0])
-        array[i][1] = float(lines[i].split(" ")[1])
-    return array
-
 
 def plan(run_time, planner_type, objective_type, wind_direction, dimensions, goal_pos, obstacles):
     # Construct the robot state space in which we're planning. We're
@@ -240,7 +68,7 @@ def plan(run_time, planner_type, objective_type, wind_direction, dimensions, goa
     # si.setStateValidityCheckingResolution(0.001)
 
     # Set the object used to check which states in the space are valid
-    validity_checker = ValidityChecker(si, obstacles)
+    validity_checker = ph.ValidityChecker(si, obstacles)
     ss.setStateValidityChecker(validity_checker)
 
     # Set our robot's starting state to be the bottom-left corner of
@@ -260,21 +88,17 @@ def plan(run_time, planner_type, objective_type, wind_direction, dimensions, goa
 
     # Create the optimization objective specified by our command-line argument.
     # This helper function is simply a switch statement.
-    objective = allocate_objective(si, objective_type)
+    objective = ph.allocate_objective(si, objective_type)
     lengthObj = ob.PathLengthOptimizationObjective(si)
-    clearObj = ClearanceObjective(si)
-    minTurnObj = MinTurningObjective(si)
-    windObj = WindObjective(si)
+    clearObj = ph.ClearanceObjective(si)
+    minTurnObj = ph.MinTurningObjective(si)
+    windObj = ph.WindObjective(si)
     ss.setOptimizationObjective(objective)
     print("ss.getProblemDefinition().hasOptimizationObjective( ){}".format(ss.getProblemDefinition().hasOptimizationObjective()))
     print("ss.getProblemDefinition().hasOptimizedSolution() {}".format(ss.getProblemDefinition().hasOptimizedSolution()))
 
-    # Create a decomposition of the state space
-    decomp = MyDecomposition(256, bounds)
-
     # Construct the optimal planner specified by our command line argument.
     # This helper function is simply a switch statement.
-    # optimizing_planner = allocate_planner(si, planner_type, decomp)
     optimizing_planner = allocatePlanner(si, planner_type)
 
     ss.setPlanner(optimizing_planner)
@@ -319,20 +143,4 @@ def plan(run_time, planner_type, objective_type, wind_direction, dimensions, goa
 
     else:
         print("No solution found.")
-
-
-def plot_path(solution_path, dimensions, obstacles):
-    matrix = solution_path.printAsMatrix()
-    path = create_numpy_path(matrix)
-    x, y = path.T
-    ax = plt.gca()
-    ax.plot(x, y, 'r--')
-    ax.plot(x, y, 'go') 
-    ax.axis(xmin=dimensions[0], xmax=dimensions[2], ymin=dimensions[1], ymax=dimensions[3])
-    for obstacle in obstacles:
-        ax.add_patch(plt.Circle((obstacle.x, obstacle.y), radius=obstacle.radius))
-
-    plt.show()
-
-
 
