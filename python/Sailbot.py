ae61842043e71353f8bdfddfb84085c73668d68e
Tyler Lum <tylergwlum@gmail.com>
2020-01-18 11:11:25 -0800

    Add wind message to Sailbot.py

--- python/Sailbot.py
+++ python/Sailbot.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
 import rospy
-from Sailbot_Custom_ROS_Message_Example.msg import AIS
+from local_pathfinding.msg import wind
+from local_pathfinding.msg import AIS
 from geometry_msgs.msg import Pose2D
 from std_msgs.msg import Float64
-
-class WindSensorData:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return "x: {0}. y: {1}".format(self.x, self.y)
 
 class GPSCoordinates:
     def __init__(self, latitude, longitude):
@@ -21,36 +14,38 @@ class GPSCoordinates:
         return "Lat: {0}. Long: {1}".format(self.latitude, self.longitude)
 
 class BoatState:
-    def __init__(self, globalWaypoint, currentPosition, windSensorData, AISData, heading):
+    def __init__(self, globalWaypoint, currentPosition, wind_direction, wind_speed, AISData, heading):
         self.globalWaypoint = globalWaypoint
         self.currentPosition = currentPosition
-        self.windSensorData = windSensorData
+        self.wind_direction = wind_direction
+        self.wind_speed = wind_speed
         self.AISData = AISData
         self.heading = heading
 
     def __str__(self):
         return (("Global waypoint: {0}\n"
                  "Current position: {1}\n"
-                 "Wind sensor: {2}\n"
-                 "AISData: {3}\n"
-                 "Heading: {4}\n")
-                 .format(self.globalWaypoint, self.currentPosition, self.windSensorData, self.AISData.MMSI, self.heading))
+                 "Wind direction: {2}\n"
+                 "Wind speed: {3}\n"
+                 "AISData: {4}\n"
+                 "Heading: {5}\n")
+                 .format(self.globalWaypoint, self.currentPosition, self.wind_direction, self.wind_speed, self.AISData.MMSI, self.heading))
 
 
 class Sailbot:
 
     def getCurrentState(self):
-        return BoatState(self.globalWaypoint, self.currentPosition, self.windSensorData, self.AISData, self.heading)
+        return BoatState(self.globalWaypoint, self.currentPosition, self.wind_direction, self.wind_speed, self.AISData, self.heading)
 
     def currentPositionCallback(self, data):
         rospy.loginfo(data)
         self.currentPosition.latitude = data.x
         self.currentPosition.longitude = data.y
     
-    def windSensorDataCallback(self, data):
+    def windCallback(self, data):
         rospy.loginfo(data)
-        self.windSensorData.x = data.x
-        self.windSensorData.y = data.y
+        self.wind_direction = data.direction
+        self.wind_speed = data.speed
 
     def AISDataCallback(self, data):
         rospy.loginfo(data)
@@ -63,13 +58,14 @@ class Sailbot:
     def __init__(self):
         self.globalWaypoint = GPSCoordinates(0, 0)
         self.currentPosition = GPSCoordinates(0, 0)
-        self.windSensorData = WindSensorData(0, 0)
+        self.wind_direction = 0
+        self.wind_speed = 0
         self.AISData = AIS()
         self.heading = 0
 
         rospy.init_node('local_pathfinding', anonymous=True)
         rospy.Subscriber("current_position", Pose2D, self.currentPositionCallback)
-        rospy.Subscriber("windSensorData", Pose2D, self.windSensorDataCallback)
+        rospy.Subscriber("MOCK_wind", wind, self.windCallback)
         rospy.Subscriber("AISData", AIS, self.AISDataCallback)
         rospy.Subscriber("heading", Float64, self.headingCallback)
 
