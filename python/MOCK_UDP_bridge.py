44566c2532b99a8e544a0b44c8881443054a881d
Håkon Vågsether <haakonsv@gmail.com>
2020-01-25 18:57:47 -0800

    Update dependencies and add TODO

--- python/MOCK_UDP_bridge.py
+++ python/MOCK_UDP_bridge.py
@@ -15,7 +15,7 @@ except ImportError as err:
     print("")
     print("You seem to be missing a dependency (or two).")
     print("Please run the following commands from within the local-pathfinding directory:")
-    print("  git submodule init && git submodule update")
+    print("  pip2 install bitstring")
     print("  pip2 install pynmea2")
     print("")
     exit()
@@ -35,12 +35,13 @@ class MOCK_UDPBridge:
 	rospy.Subscriber("MOCK_AIS", msg.AIS_msg, self.aisCallback)
 
     def gpsCallback(self, data):
-        # TODO: fix timestamp?
+        # TODO: fix heading
         rospy.loginfo(data)
         nmea_msg = nmea.RMC('GP', 'RMC', ('000000', 'A', str(data.lat*100)[0:7], 'N', str(abs(data.lon)*100)[0:7], 'W', '2.0', '0.0', '250120', '000.0', 'W'))
         sock.sendto(str(nmea_msg), (UDP_IP, UDP_PORT))
 
     def aisCallback(self, data):
+	# TODO: fix heading
         rospy.loginfo(data)
         for ship in data.ships:
             aisreport = ais.AISPositionReportMessage(mmsi=ship.ID, lon=int(ship.lon*600000), lat=int(ship.lat*600000), heading=int(-ship.heading*90 - 180) % 360)
