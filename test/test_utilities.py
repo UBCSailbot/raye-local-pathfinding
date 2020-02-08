fdcb3ce5526a1a21537b02ecd856f015202d9fc7
Tyler Lum <tylergwlum@gmail.com>
2020-02-08 13:08:35 -0800

    Add test for getDesiredHeading

--- test/test_utilities.py
+++ test/test_utilities.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+# Add python directory to path
 import sys, os
 testdir = os.path.dirname(__file__)
 srcdir = '../python'
@@ -9,8 +10,8 @@ import unittest
 import rostest
 from local_pathfinding.msg import latlon, AIS_ship
 from utilities import *
+from geopy.distance import distance
 
-threshold = 0.001
 class TestUtilities(unittest.TestCase):
     def test_latlonToXY_and_XYToLatlon_advanced(self):
         # Setup latlons
@@ -42,6 +43,39 @@ class TestUtilities(unittest.TestCase):
         calculatedLatlon = XYToLatlon(xy=xy, referenceLatlon=testLatlon)
         self.assertAlmostEqual(calculatedLatlon.lat, testLatlon.lat, places=1)  # calculatedLatlon should be same as testLatlon
         self.assertAlmostEqual(calculatedLatlon.lon, testLatlon.lon, places=1)
+
+    def test_getDesiredHeading_north(self):
+        # Heading is defined using cartesian coordinates. 0 degrees is East. 90 degrees in North. 270 degrees is South.
+        # Bearing is defined differently. 0 degrees is North. 90 degrees is East. 180 degrees is South.
+        # Heading = -Bearing + 90
+
+        # Setup latlon and bearing
+        northBearing = 0
+        position = latlon(50, -120)
+
+        # Setup destination
+        positionToLocalWaypointDistance = distance(kilometers=1)
+        destination = positionToLocalWaypointDistance.destination(point=(position.lat, position.lon), bearing=northBearing)
+        localWaypoint = latlon(destination.latitude, destination.longitude)
+
+        # Test desiredHeading
+        desiredHeading = getDesiredHeading(position=position, localWaypoint=localWaypoint)
+        self.assertAlmostEqual(desiredHeading, -northBearing+90, places=1)
+
+    def test_getDesiredHeading_east(self):
+        # Setup latlon and bearing
+        eastBearing = 90
+        position = latlon(50, -120)
+
+        # Setup destination
+        positionToLocalWaypointDistance = distance(kilometers=1)
+        destination = positionToLocalWaypointDistance.destination(point=(position.lat, position.lon), bearing=eastBearing)
+        localWaypoint = latlon(destination.latitude, destination.longitude)
+
+        # Test desiredHeading
+        desiredHeading = getDesiredHeading(position=position, localWaypoint=localWaypoint)
+        self.assertAlmostEqual(desiredHeading, -eastBearing+90, places=1)
+
 
 if __name__ == '__main__':
     rostest.rosrun('local_pathfinding', 'test_utilities', TestUtilities)
