37b1be71cdbeb4e7cec5386079a515434844f1e4
Håkon Vågsether <hakon.vagsether@gmail.com>
2020-02-15 22:24:12 +0000

    Add real ships from exactAIS

--- python/MOCK_AIS.py
+++ python/MOCK_AIS.py
@@ -2,6 +2,8 @@
 import rospy
 import random
 import json
+import urllib2
+import time
 
 from std_msgs.msg import Float64
 from geopy.distance import distance
@@ -69,13 +71,26 @@ class Ship:
             self.id,
             self.lat,
             self.lon,
-            self.headingDegrees,
+            (90.0 -self.headingDegrees) % 360,
             self.speedKmph)
+
+class RealShip(Ship):
+    def __init__(self, MMSI, lat, lon, heading, speed):
+        self.id = MMSI
+        self.lat = lat
+        self.lon = lon
+        self.headingDegrees = heading
+        self.speedKmph = speed
 
 
 class MOCK_AISEnvironment:
     # Just a class to keep track of the ships surrounding the sailbot
     def __init__(self, lat, lon, ais_file):
+        self.use_real_ships = rospy.get_param('use_real_ships', default=False)
+        self.token = rospy.get_param('AIS_token', default='')
+        self.sailbot_lat = lat
+        self.sailbot_lon = lon
+
         # Setup ros objects
         rospy.init_node('MOCK_AIS', anonymous=True)
         self.publisher = rospy.Publisher("AIS", AISMsg, queue_size=4)
@@ -88,22 +103,25 @@ class MOCK_AISEnvironment:
         self.set_random_seed()
 
         # Create ships
+        self.last_real_ship_pull = time.time() # The timestamp for the last time we downloaded new ship positions
         self.publishPeriodSeconds = AIS_PUBLISH_PERIOD_SECONDS
         self.speedup = 1.0
         self.ships = []
+
         if ais_file:
             f = open(ais_file, 'r')
             ship_list = json.load(f)
             self.numShips = len(ship_list)
             for ship in ship_list:
                 self.ships.append(Ship(*ship, publishPeriodSeconds=self.publishPeriodSeconds))
+        elif self.use_real_ships:
+            self.last_real_ship_pull = time.time() # The timestamp for the last time we downloaded new ship positions
+            self.get_real_ships()
         else:
             self.numShips = NUM_AIS_SHIPS
             for i in range(self.numShips):
                 self.ships.append(RandomShip(i, lat, lon, self.publishPeriodSeconds))
 
-        self.sailbot_lat = lat
-        self.sailbot_lon = lon
 
     def set_random_seed(self):
         randomSeed = rospy.get_param('random_seed', "")
@@ -125,10 +143,13 @@ class MOCK_AISEnvironment:
                     self.ships.insert(i, RandomShip(i, self.sailbot_lat, self.sailbot_lon, self.publishPeriodSeconds))
 
     def make_ros_message(self):
-        rospy.loginfo([ship.id for ship in self.ships])
         ship_list = []
-        for i in range(self.numShips):
-            ship_list.append(self.ships[i].make_ros_message())
+        if self.use_real_ships:
+            for ship in self.real_ships:
+                ship_list.append(ship.make_ros_message())
+        else:
+            for i in range(self.numShips):
+                ship_list.append(self.ships[i].make_ros_message())
         return AISMsg(ship_list)
 
     def new_boat_callback(self, msg):
@@ -146,6 +167,47 @@ class MOCK_AISEnvironment:
     def speedup_callback(self, msg):
         self.speedup = msg.data
 
+    def get_real_ships(self):
+        lat = self.sailbot_lat
+        lon = self.sailbot_lon
+        space = "%20"
+        pos_list = ""
+        pos_list += str(lon-0.2) + space + str(lat-0.2) + space # bot left
+        pos_list += str(lon-0.2) + space + str(lat+0.2) + space # top left
+        pos_list += str(lon+0.2) + space + str(lat+0.2) + space # top right
+        pos_list += str(lon+0.2) + space + str(lat-0.2) + space # bot right
+        pos_list += str(lon-0.2) + space + str(lat-0.2)         # bot left
+
+
+        url = 'https://services.exactearth.com/gws/wms?service=WFS&version=1.1.0&authKey=' + self.token
+        url += '&request=GetFeature&typeName=exactAIS:LVI&outputFormat=json&filter=<Filter%20xmlns:gml="http://www.opengis.net/gml">'
+        url += '<Intersects><PropertyName>position</PropertyName><gml:Polygon%20xmlns:gml="http://www.opengis.net/gml"%20srsName="EPSG:4326">'
+        url += '<gml:exterior><gml:LinearRing><gml:posList>' + pos_list 
+        url += '</gml:posList></gml:LinearRing></gml:exterior></gml:Polygon></Intersects></Filter>'
+        request = urllib2.Request(url)
+        # Set the user agent to something so our request is accepted
+        #request.add_header("User-Agent", "Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0")
+        #request.add_header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0")
+        #request.add_header("User-Agent", "Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36")
+        print("Querying the server")
+        data = urllib2.urlopen(request)
+        data = data.read()
+        if data.startswith('{\n"errors"'):
+            print("Queried the server too often, waiting for 2 minutes")
+            print(data)
+            return
+        ships = json.loads(data)
+        self.real_ships = []
+        for real_ship in ships['features']:
+            # Speed is given in knots
+            real_ship = real_ship[u'properties']
+            new_ship = RealShip(int(real_ship[u'mmsi']),
+                                float(real_ship[u'latitude']),
+                                float(real_ship[u'longitude']),
+                                float(real_ship[u'cog']),
+                                float(real_ship[u'sog']) * 0.54)
+            self.real_ships.append(new_ship)
+
 
 if __name__ == '__main__':
     # Get ais_file parameter
@@ -155,7 +217,17 @@ if __name__ == '__main__':
     r = rospy.Rate(1.0 / ais_env.publishPeriodSeconds)  # hz
 
     while not rospy.is_shutdown():
+        #rospy.loginfo(ais_env.real_ships)
+        if ais_env.use_real_ships:
+            timestamp = time.time()
+            # If it's been more than two minutes since last time we downloaded real ship
+            # positions, do it again
+            if timestamp - ais_env.last_real_ship_pull > 120:
+                ais_env.last_real_ship_pull = timestamp
+                ais_env.get_real_ships()
+        else:
+            ais_env.move_ships()
+
         data = ais_env.make_ros_message()
-        ais_env.move_ships()
         ais_env.publisher.publish(data)
         r.sleep()
