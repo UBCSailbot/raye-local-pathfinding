d8fe191c67575b87d1bbc7ccf836b304c637b690
Tyler Lum <tylergwlum@gmail.com>
2020-02-27 17:31:11 -0800

    Global wind changes speed and direction after a given time period (#59)
    * Global wind changes speed and direction after a given time period
    
    * Increase period to 10min

--- python/MOCK_wind_sensor.py
+++ python/MOCK_wind_sensor.py
@@ -4,6 +4,14 @@ import math
 
 from local_pathfinding.msg import windSensor, GPS
 from utilities import globalWindToMeasuredWind
+import random
+
+# Constants
+WIND_PUBLISH_PERIOD_SECONDS = 15.0
+MAX_GLOBAL_WIND_SPEED_KMPH = 10
+MIN_GLOBAL_WIND_SPEED_KMPH = 1
+CHANGE_WIND_DIRECTION_PERIOD_SECONDS = 600
+CHANGE_WIND_DIRECTION_AMOUNT_DEGREES = 45
 
 # Global variables for tracking boat speed
 boatHeadingDegrees = 0
@@ -20,13 +28,26 @@ def talker():
 
     pub = rospy.Publisher('windSensor', windSensor, queue_size=4)
     rospy.init_node('MOCK_wind_talker', anonymous=True)
-    r = rospy.Rate(1) #1 HZ
+    r = rospy.Rate(1.0 / WIND_PUBLISH_PERIOD_SECONDS)
     msg = windSensor()
 
+    # Set initial global wind conditions
+    globalWindSpeedKmph = random.randint(MIN_GLOBAL_WIND_SPEED_KMPH, MAX_GLOBAL_WIND_SPEED_KMPH)
+    globalWindDirectionDegrees = 90
+
+    # Change wind counter
+    changeWindCounter = 0
+    changeWindMax = int(CHANGE_WIND_DIRECTION_PERIOD_SECONDS / WIND_PUBLISH_PERIOD_SECONDS)
     while not rospy.is_shutdown():
-        # Set windSensor message such that the global wind is 10km/h in 90 degrees direction
-        globalWindSpeedKmph = 10
-        globalWindDirectionDegrees = 90
+        # Update global wind speed and direction
+        if changeWindCounter >= changeWindMax:
+            changeWindCounter = 0
+            globalWindSpeedKmph = random.randint(MIN_GLOBAL_WIND_SPEED_KMPH, MAX_GLOBAL_WIND_SPEED_KMPH)
+            globalWindDirectionDegrees += CHANGE_WIND_DIRECTION_AMOUNT_DEGREES
+        else:
+            changeWindCounter += 1
+
+        # Set windSensor message such that the global wind matches the variables
         measuredWindSpeedKmph, measuredWindDirectionDegrees = globalWindToMeasuredWind(globalWindSpeedKmph, globalWindDirectionDegrees, boatSpeedKmph, boatHeadingDegrees)
         msg.measuredDirectionDegrees = measuredWindDirectionDegrees
         msg.measuredSpeedKmph = measuredWindSpeedKmph
