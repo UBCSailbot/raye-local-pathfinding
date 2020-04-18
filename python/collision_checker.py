cf348e8285682e1bb2a0ceae87791fa9dffcf65e
Zephaniah <zephaniahko@gmail.com>
2020-04-18 13:57:45 -0700

    added km labels, more detailed warn messages

--- python/collision_checker.py
+++ python/collision_checker.py
@@ -4,14 +4,14 @@ from local_pathfinding.msg import AISMsg, GPS, path, latlon, windSensor
 from geopy.distance import distance
 from MOCK_AIS import AIS_PUBLISH_PERIOD_SECONDS
 
-COLLISION_RADIUS = 0.1
-WARN_RADIUS = 0.3
+COLLISION_RADIUS_KM = 0.1
+WARN_RADIUS_KM = 0.3
 CHECK_PERIOD = AIS_PUBLISH_PERIOD_SECONDS 
 
 class CollisionChecker:
-    def __init__(self, collision_radius, warn_radius):
-        self.collision_radius = collision_radius
-        self.warn_radius = warn_radius
+    def __init__(self, collision_radius_km, warn_radius_km):
+        self.collision_radius = collision_radius_km
+        self.warn_radius = warn_radius_km
         rospy.init_node('collision_checker', anonymous=True)
         rospy.Subscriber('/AIS', AISMsg, self.AIS_callback)
         rospy.Subscriber('/GPS', GPS, self.GPS_callback)
@@ -28,15 +28,15 @@ class CollisionChecker:
 
     def check_for_collisions(self):
         for ship in self.ships:
-            dist = distance((ship.lat, ship.lon), (self.lat, self.lon)) 
+            dist = distance((ship.lat, ship.lon), (self.lat, self.lon)).km
             if dist < self.collision_radius:
-                rospy.logfatal("Boat has collided with obstacle")
+                rospy.logfatal("Boat has collided with obstacle. Collision radius {}km. Actual distance to boat: {}km".format(self.collision_radius, dist))
 
             elif dist < self.warn_radius:
-                rospy.logwarn("Sailbot is within {}km of an obstacle. Close to collision".format(self.warn_radius))
+                rospy.logwarn("Close to collision. Within {}km of boat. Actual distance to boat: {}km".format(self.warn_radius, dist))
 
 if __name__=='__main__':
-    collision_checker = CollisionChecker(COLLISION_RADIUS, WARN_RADIUS)
+    collision_checker = CollisionChecker(COLLISION_RADIUS_KM, WARN_RADIUS_KM)
     rate = rospy.Rate(1 / CHECK_PERIOD)
 
     while not rospy.is_shutdown():
