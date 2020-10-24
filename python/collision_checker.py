6db116b4f350d44d3ec7d743a02963b876406a3b
Patrick <pat5546@student.ubc.ca>
2020-10-24 12:21:34 -0700

    number of warnings and collisions print on shutdown

--- python/collision_checker.py
+++ python/collision_checker.py
@@ -33,27 +33,26 @@ class CollisionChecker:
         check_ship = colliding_ship
 
         if check_ship is None:
-            for ship in self.ships:            
+            for ship in self.ships:
                 dist = distance((ship.lat, ship.lon), (self.lat, self.lon)).km
 
                 if dist < self.collision_radius:
                     check_ship = ship
                     self.times_collided = self.times_collided + 1
-                    rospy.logfatal("Boat has collided with obstacle. Collision radius {}km. "
-                                   "Actual distance to boat: {}km. Collision number: {}"
+                    rospy.logfatal("Boat has collided with obstacle. Collision radius {}km.\n"
+                                   "\tActual distance to boat: {}km. Collision number: {}"
                                    .format(self.collision_radius, dist, self.times_collided))
 
                 elif dist < self.warn_radius:
                     check_ship = ship
                     self.times_warned = self.times_warned + 1
-                    rospy.logwarn("Close to collision. Within {}km of boat. Actual distance to boat: {}km. "
-                                  "Warning number: {}"
-                                  .format(self.warn_radius, dist, self.times_warned))
+                    rospy.logwarn("Close to collision. Within {}km of boat.\n\tActual distance to boat: {}km. "
+                                  "Warning number: {}" .format(self.warn_radius, dist, self.times_warned))
         else:
             dist = distance((check_ship.lat, check_ship.lon), (self.lat, self.lon)).km
             if not (dist < self.collision_radius or dist < self.warn_radius):
                 check_ship = None
-                rospy.logwarn("Boat is not in collision or warning radius anymore.")
+                rospy.logwarn("Other ships are not within the collision or warning radius anymore.")
 
         return check_ship
 
@@ -66,6 +65,7 @@ class CollisionChecker:
 
 if __name__ == '__main__':
     collision_checker = CollisionChecker(COLLISION_RADIUS_KM, WARN_RADIUS_KM)
+
     rate = rospy.Rate(1 / CHECK_PERIOD)
 
     shipColliding = None
@@ -73,10 +73,5 @@ if __name__ == '__main__':
         shipColliding = collision_checker.check_for_collisions(shipColliding)
         rate.sleep()
 
-    rospy.loginfo("Number of collisions: {}. Number of warnings: {}."
-                  .format(collision_checker.get_times_collided(), collision_checker.get_times_warned()))
-
-
-# To do:
-# - How to get the last message to print to screen, log, and rosout?
-# - How to save screenshot?
+    print("Number of collisions: {}. Number of warnings: {}."
+          .format(collision_checker.get_times_collided(), collision_checker.get_times_warned()))
