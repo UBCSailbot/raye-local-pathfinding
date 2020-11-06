609bb63ebccdc91fef9bd27949b78cac58d29de7
Patrick <pat5546@student.ubc.ca>
2020-11-05 22:13:45 -0800

    finished

--- python/log_closest_obstacle.py
+++ python/log_closest_obstacle.py
@@ -2,15 +2,24 @@
 import rospy
 from geopy.distance import distance
 from local_pathfinding.msg import AISMsg, GPS
-# import os
-# import local_pathfinding.msg as msg
-# from datetime import datetime
-# from datetime import date
-# import pickle
-# import matplotlib.pyplot as plt
-# import numpy as np
+import os
+import local_pathfinding.msg as msg
+from std_msgs.msg import String, Float64
+from datetime import datetime
+from datetime import date
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
 
 UPDATE_TIME_SECONDS = 1.0
+
+# Output paths
+dateStr = date.today().strftime("%b-%d-%Y")
+timeStr = datetime.now().strftime('%H-%M-%S')
+ABS_PATH_TO_THIS_FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+ABS_PATH_TO_OUTPUT_DIR = os.path.join(ABS_PATH_TO_THIS_FILE_DIR, "../output/{}_{}".format(dateStr, timeStr))
+ABS_PATH_TO_OUTPUT_MIN_DISTANCE_FILE = os.path.join(ABS_PATH_TO_OUTPUT_DIR, "minDistance.pkl")
+ABS_PATH_TO_OUTPUT_MIN_DISTANCE_PLOT_FILE = os.path.join(ABS_PATH_TO_OUTPUT_DIR, "min_distance_plot.png")
 
 class LogClosestObstacle:
     def __init__(self):
@@ -26,16 +35,26 @@ class LogClosestObstacle:
     def get_closest_distances(self):
         return self.closestDistances
 
+    def AIS_callback(self, data):
+        self.ships = data.ships
+
+    def GPS_callback(self, data):
+        self.lat = data.lat
+        self.lon = data.lon
+
     def find_closest_ship(self):
-        closet_ship_dist = None
+        closest_ship_dist = None
 
         for ship in self.ships:
             dist = distance((ship.lat, ship.lon), (self.lat, self.lon)).km
 
-            if closet_ship_dist is None or closet_ship_dist > dist:
+            if closest_ship_dist is None or closest_ship_dist > dist:
                 closest_ship_dist = dist
 
-        self.closestDistances.append(closet_ship_dist)
+        if closest_ship_dist is None or len(self.closestDistances) == 0:
+            self.closestDistances.append(0)
+        else:
+            self.closestDistances.append(closest_ship_dist)
 
 
 if __name__ == '__main__':
@@ -46,56 +65,20 @@ if __name__ == '__main__':
         log_closest_obstacle.find_closest_ship()
         rate.sleep()
 
-    # # Save stored path information to pickle files
-    # if not os.path.exists(ABS_PATH_TO_OUTPUT_DIR):
-    #     os.makedirs(ABS_PATH_TO_OUTPUT_DIR)
-    # with open(ABS_PATH_TO_OUTPUT_PATH_FILE, 'wb') as handle:
-    #     pickle.dump(path_storer.paths, handle)
-    # with open(ABS_PATH_TO_OUTPUT_PATH_COST_FILE, 'wb') as handle:
-    #     pickle.dump(path_storer.pathCosts, handle)
-    # with open(ABS_PATH_TO_OUTPUT_PATH_COST_BREAKDOWN_FILE, 'wb') as handle:
-    #     pickle.dump(path_storer.pathCostBreakdowns, handle)
+    # Save stored path information to pickle files
+    if not os.path.exists(ABS_PATH_TO_OUTPUT_DIR):
+        os.makedirs(ABS_PATH_TO_OUTPUT_DIR)
+    with open(ABS_PATH_TO_OUTPUT_MIN_DISTANCE_FILE, 'wb') as handle:
+        pickle.dump(log_closest_obstacle.get_closest_distances(), handle)
 
-    # # Plot Path cost vs. Time
-    # rospy.loginfo("About to save cost plot...")
-    # indices = np.arange(len(path_storer.pathCosts))
-    # times = CHECK_PERIOD_SECONDS * indices
-    # plt.figure()
-    # plt.plot(times, path_storer.pathCosts)
-    # plt.xlabel("Time elapsed (s)")
-    # plt.ylabel("Path cost")
-    # plt.title("Path cost vs. Time")
-    # plt.savefig(ABS_PATH_TO_OUTPUT_COST_PLOT_FILE)
-    # rospy.loginfo("Successfully saved cost plot to {}".format(ABS_PATH_TO_OUTPUT_COST_PLOT_FILE))
-
-    # # Plot Path cost breakdown vs. Time
-    # rospy.loginfo("About to save cost breakdown plot...")
-    # # Get objective names, except for the "Multi" objective, which is the total cost (we want breakdown)
-    # objectives = [x for x in path_storer.pathCostBreakdowns[0].split(" ") if "Objective" in x and "Multi" not in x]
-
-    # # Get the cost from each objective
-    # costBreakdowns = [[] for _ in objectives]
-    # for costBreakdownString in path_storer.pathCostBreakdowns:
-    #     costBreakdownStringSplit = costBreakdownString.split(" ")
-
-    #     # Get weighted cost. Will be in form: [..., 'Weighted', 'cost', '=', '79343.0', ...]
-    #     costBreakdown = [float(costBreakdownStringSplit[i+3]) for i in range(len(costBreakdownStringSplit))
-    #                      if costBreakdownStringSplit[i] == "Weighted"]
-    #     for i in range(len(costBreakdown)):
-    #         costBreakdowns[i].append(costBreakdown[i])
-
-    # # Create stacked bar chart. Need to store bottoms to properly stack.
-    # plt.figure()
-    # bottoms = np.array([0.0] * len(costBreakdowns[0]))
-    # barPlots = []
-    # for costBreakdown in costBreakdowns:
-    #     barPlot = plt.bar(times, costBreakdown, bottom=bottoms, width=CHECK_PERIOD_SECONDS/2)
-    #     barPlots.append(barPlot)
-    #     bottoms += np.array(costBreakdown)
-
-    # plt.xlabel("Time elapsed (s)")
-    # plt.ylabel("Path cost")
-    # plt.title("Path cost vs. Time")
-    # plt.legend([bPlot[0] for bPlot in barPlots], objectives)
-    # plt.savefig(ABS_PATH_TO_OUTPUT_COST_BREAKDOWN_PLOT_FILE)
-    # rospy.loginfo("Successfully saved cost breakdown plot to {}".format(ABS_PATH_TO_OUTPUT_COST_BREAKDOWN_PLOT_FILE))
+    # Plot Path cost vs. Time
+    rospy.loginfo("About to save closest distance plot...")
+    indices = np.arange(len(log_closest_obstacle.get_closest_distances()))
+    times = UPDATE_TIME_SECONDS * indices
+    plt.figure()
+    plt.plot(times, log_closest_obstacle.get_closest_distances())
+    plt.xlabel("Time elapsed (s)")
+    plt.ylabel("Closest Distances (km)")
+    plt.title("Distance to Closest AIS Boat vs. Time")
+    plt.savefig(ABS_PATH_TO_OUTPUT_MIN_DISTANCE_PLOT_FILE)
+    rospy.loginfo("Successfully saved closest distance plot to {}".format(ABS_PATH_TO_OUTPUT_MIN_DISTANCE_PLOT_FILE))
