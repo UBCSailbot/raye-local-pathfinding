3642ebbaaa945758c1a23b52ada918e78d772911
Patrick <pat5546@student.ubc.ca>
2020-11-15 01:06:44 -0800

    used argument

--- python/min_dist_graphing.py
+++ python/min_dist_graphing.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pickle as pl
 import os
+import sys
 
 # match with collision_checker.py
 COLLISION_RADIUS_KM = 0.1
@@ -17,14 +18,10 @@ def plot_min_distances(folder_name):
     x, y = fig_handle.axes[0].lines[0].get_data()
 
     # Getting warning times
-    i = 1  # i = 0 is y intercept, not wanted
     warn_start_times = list()
-    while i < len(y):
-        if y[i] < WARN_RADIUS_KM:
+    for i in range(1, len(y)):  # i = 0 is unwanted, y-intercept
+        if y[i] < WARN_RADIUS_KM and y[i - 1] > y[i]:
             warn_start_times.append(x[i])
-            while i + 1 < len(y) and y[i + 1] >= y[i]:
-                i += 1
-        i += 1
 
     print(warn_start_times)
 
@@ -33,8 +30,8 @@ def plot_min_distances(folder_name):
         pl.load(open(PATH_TO_PKL, 'rb'))
         print ("Warning around {} seconds" .format(warning))
         plt.ylim(0, 1)
-        plt.xlim(warning - 10, warning + 10)
+        plt.xlim(warning - 5, warning + 5)
         plt.show()
 
 
-plot_min_distances("Nov-12-2020_18-30-40")
+plot_min_distances(sys.argv[1])
