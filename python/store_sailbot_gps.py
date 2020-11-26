504c0e315e8f366550da8ddaef28db32c4617e0e
Joshua <joshuasam@shaw.ca>
2020-11-25 20:26:37 -0800

    fixing flake8

--- python/store_sailbot_gps.py
+++ python/store_sailbot_gps.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 
 import rospy
-import numpy as np
 import os
 from geopy.distance import distance
 import matplotlib.pyplot as plt
-from sailbot_msg.msg import AISMsg, GPS
+from sailbot_msg.msg import GPS
 from datetime import datetime
 from datetime import date
 
-#constants
+# Constants
 UPDATE_TIME_SECONDS = 1.0
 
 dateStr = date.today().strftime("%b-%d-%Y")
@@ -18,28 +17,30 @@ ABS_PATH_TO_THIS_FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 ABS_PATH_TO_OUTPUT_DIR = os.path.join(ABS_PATH_TO_THIS_FILE_DIR, "../output/{}_{}".format(dateStr, timeStr))
 ABS_PATH_TO_OUTPUT_SAILBOT_GPS_DISPLACEMENT = os.path.join(ABS_PATH_TO_OUTPUT_DIR, "sailbot_gps_plot.png")
 
+
 class SailbotGPSData:
     def __init__(self):
-        #Subscribe to the GPS data, invoke the callback function every time the data is changed
+        rospy.init_node('SailbotGPSData')
+        # Subscribe to the GPS data, invoke the callback function every time the data is changed
         rospy.Subscriber('/GPS', GPS, self.GPS_callback)
 
-        #Get initial values for Lat and Lon and simulation start time
+        # Get initial values for Lat and Lon and simulation start time
         self.latInitial = rospy.wait_for_message('/GPS', GPS).lat
         self.lonInitial = rospy.wait_for_message('/GPS', GPS).lon
 
-        #array to hold all the Lat and Lon values
+        # Array to hold all the Lat and Lon values
         self.latArray = list()
         self.lonArray = list()
 
-        #place initial values into the Lat and Lon array
+        # Place initial values into the Lat and Lon array
         self.latArray.append(self.latInitial)
         self.lonArray.append(self.lonInitial)
 
-        #wait some time for the other processes to start up
+        # Wait some time for the other processes to start up
         rospy.sleep(5)
 
     def GPS_callback(self, data):
-        #Update the values for Lat and Lon
+        # Update the values for Lat and Lon
         self.lat = data.lat
         self.lon = data.lon
 
@@ -47,18 +48,15 @@ class SailbotGPSData:
         return distance((self.latInitial, self.lonInitial), (self.lat, self.lon)).km
 
     def StoreSailbotGPS(self):
-        #sleep for 1 second, so that lat and lon values are updated every second
-        rospy.sleep(UPDATE_TIME_SECONDS)
-
-        #add the new values for the lat and lon to the array
+        # Add the new values for the lat and lon to the array
         self.latArray.append(self.lat)
         self.lonArray.append(self.lon)
 
-        #using geopy, return distance from start position in km and log to constole
+        # Using geopy, return distance from start position in km and log to constole
         self.distance = str(self.Find_Distance())
 
-        #uncomment this code if you want to see the displacement plot in real time
-        ''' 
+        # Uncomment this code if you want to see the displacement plot in real time
+        '''
         plt.plot(self.lonArray, self.latArray, 'ro')
         plt.title('displacement in km:' + self.distance)
         plt.grid()
@@ -68,16 +66,20 @@ class SailbotGPSData:
         plt.pause(0.0001)
         plt.clf()
         '''
-    
+
+
 if __name__ == '__main__':
-    rospy.init_node('SailbotGPSData')
+
     myClass = SailbotGPSData()
     while not rospy.is_shutdown():
         myClass.StoreSailbotGPS()
-    
+
+        # Sleep for 1 second, so that lat and lon values are updated every second
+        rospy.sleep(UPDATE_TIME_SECONDS)
+
     rospy.loginfo("Saving to {}".format(ABS_PATH_TO_OUTPUT_SAILBOT_GPS_DISPLACEMENT))
 
-    #create plot and save plot to destination located at ABS_PATH_TO_OUTPUT_SAILBOT_GPS_DISPLACEMENT
+    # Create plot and save plot to destination located at ABS_PATH_TO_OUTPUT_SAILBOT_GPS_DISPLACEMENT
     plt.figure()
     plt.plot(myClass.lonArray, myClass.latArray, 'ro')
     plt.title('displacement in km:' + myClass.distance)
@@ -85,5 +87,4 @@ if __name__ == '__main__':
     plt.xlabel('Longitude')
     plt.ylabel('Latitude')
     plt.savefig(ABS_PATH_TO_OUTPUT_SAILBOT_GPS_DISPLACEMENT)
-
     rospy.loginfo("Successfully saved GPS displacement plot to {}".format(ABS_PATH_TO_OUTPUT_SAILBOT_GPS_DISPLACEMENT))
