530a1a4df5eb0759c6b8ceecb8fe076ccfd585b2
Patrick <pat5546@student.ubc.ca>
2020-11-28 12:15:32 -0800

    added new true heading field

--- python/ros_interace.py
+++ python/ros_interace.py
@@ -10,8 +10,7 @@ ERROR = 0.2  # If signals need different margin of errors, modify as required
 # - wind_speedKmph, wind_direction, wind_headingDegrees, gps_speedKmph: int32 -> float64 (no loss of precision)
 # - wind_speedKmph, gps_speedKmph: knots -> km/h
 # - wind_direction: no conversion required (0 is bow, 90 is starboard/right, etc - cw - degrees)
-# - heading_degrees: ??? -> (0 is east, 90 is north, etc - ccw - degrees)
-#     - magnetic variation: angle between magnetic and true north, where east difference is positive - cw - degrees
+# - heading_degrees: (0 is north, 90 is east, etc - cw - degrees) -> (0 is east, 90 is north, etc - ccw - degrees)
 
 
 class RosInterface:
@@ -33,35 +32,42 @@ class RosInterface:
         self.data = data
 
     def pub(self):
-        translate()
+        self.translate()
         self.pubGPS.publish(self.gps_lat, self.gps_lon, self.gps_headingDegrees, self.gps_speedKmph)
         self.pubWind.publish(self.wind_direction, self.wind_speed)
 
     # Translate from many sensor outputs to one signal, checking error margin, averaging, and converting units
     def translate(self):
-        # GPS sensors (missing headingDegrees)
-        self.gps_lat = checkErrAndAvg([self.data.gps_0_latitude, self.data.gps_1_latitude])
-        self.gps_lon = checkErrAndAvg([self.data.gps_0_longitude, self.data.gps_1_longitude])
-        self.gps_speedKmph = checkErrAndAvg([KNOTS_TO_KMPH * self.data.gps_0_groundspeed,
-                                             KNOTS_TO_KMPH * self.data.gps_1_groundspeed])
+        # GPS sensors
+        self.gps_lat = self.checkErrAndAvg([self.data.gps_0_latitude, self.data.gps_1_latitude])
+        self.gps_lon = self.checkErrAndAvg([self.data.gps_0_longitude, self.data.gps_1_longitude])
+        self.gps_headingDegrees = self.checkErrAndAvg([self.convertDegrees(self.data.gps_0_true_heading),
+                                                       self.convertDegrees(self.data.gps_1_true_heading)])
+        self.gps_speedKmph = self.checkErrAndAvg([KNOTS_TO_KMPH * self.data.gps_0_groundspeed,
+                                                  KNOTS_TO_KMPH * self.data.gps_1_groundspeed])
 
         # Wind sensors
-        self.wind_speed = checkErrAndAvg([KNOTS_TO_KMPH * self.data.wind_sensor_0_speed,
-                                          KNOTS_TO_KMPH * self.data.wind_sensor_1_speed,
-                                          KNOTS_TO_KMPH * self.data.wind_sensor_2_speed])
-        self.wind_direction = checkErrAndAvg([self.data.wind_sensor_0_direction,
-                                              self.data.wind_sensor_1_direction,
-                                              self.data.wind_sensor_2_direction])
+        self.wind_speed = self.checkErrAndAvg([KNOTS_TO_KMPH * self.data.wind_sensor_0_speed,
+                                               KNOTS_TO_KMPH * self.data.wind_sensor_1_speed,
+                                               KNOTS_TO_KMPH * self.data.wind_sensor_2_speed])
+        self.wind_direction = self.checkErrAndAvg([self.data.wind_sensor_0_direction,
+                                                   self.data.wind_sensor_1_direction,
+                                                   self.data.wind_sensor_2_direction])
+
+    # (0 is north, 90 is east, etc - cw - degrees) -> (0 is east, 90 is north, etc - ccw - degrees)
+    def convertDegrees(self, degree):
+        converted = -1 * degree + 90
+        return converted if converted >= 0 else converted + 360
 
     def checkErrAndAvg(self, arr):
-        err_free_arr = checkError(arr)
+        # err_free_arr = checkError(arr)
+        err_free_arr = arr
         return float(sum(err_free_arr)) / len(err_free_arr)
 
-    def checkError(self, arr):
-        # define constants for each signal's error of margin (compare sensor data)
-        # if within error of margin, average values
-        # returns array
-        # modifies a boolean if error is found?
+    # def checkError(self, arr):
+    #     # input is an array of values
+    #     # returns array with values outside error of margin removed
+    #     # possible to modify a boolean if error is found?
 
 
 if __name__ == "__main__":
