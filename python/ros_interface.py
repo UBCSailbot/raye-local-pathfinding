afcf58290b075d75a6a3511a0f9c3f373d05883b
Patrick <pat5546@student.ubc.ca>
2021-05-29 10:15:45 -0700

    Update sailbot-msg.msg field names

--- python/ros_interface.py
+++ python/ros_interface.py
@@ -1,14 +1,14 @@
 import rospy
-from sailbot_msg.msg import Sensors, windSensor, GPS
+from sailbot_msg.msg import Sensors, windSensor, GPS  # does the sailbot_msg name need to be updated?
 
 # Constants
 CHECK_PERIOD_SECONDS = 0.1  # How often fields are updated
 KNOTS_TO_KMPH = 1.852
-ERROR = 0.2  # If signals need different margin of errors, modify as required
+ERROR = 0.2  # If signals need different margin of errors; modify as required
 
 """
 Conversion Notes:
-- wind_speedKmph, wind_direction, wind_headingDegrees, gps_speedKmph: int32 -> float64 (no loss of precision)
+- Sensors data types are int32 or float32, GPS and windSensor are float64, so no loss of precision
 - wind_speedKmph, gps_speedKmph: knots -> km/h
 - wind_direction: (0 is forward, 90 is right, etc - cw - degrees) -> (0 is right, 90 is bow, etc - ccw - degrees)
 - heading_degrees: (0 is north, 90 is east, etc - cw - degrees) -> (0 is east, 90 is north, etc - ccw - degrees)
@@ -24,19 +24,19 @@ class RosInterface:
         self.initialized = False
         self.data = None
 
-        self.wind_speedKmph = None
-        self.wind_direction = None
         self.gps_lat = None
         self.gps_lon = None
         self.gps_headingDegrees = None
         self.gps_speedKmph = None
+        self.wind_speedKmph = None
+        self.wind_direction = None
 
-        self.past_wind_speedKmph = None
-        self.past_wind_direction = None
         self.past_gps_lat = None
         self.past_gps_lon = None
         self.past_gps_headingDegrees = None
         self.past_gps_speedKmph = None
+        self.past_wind_speedKmph = None
+        self.past_wind_direction = None
 
     def sensorsCallback(self, data):
         if not self.initialized:
@@ -63,26 +63,28 @@ class RosInterface:
         self.past_wind_speedKmph = self.wind_speedKmph
         self.past_wind_direction = self.wind_direction
 
-        # GPS sensors
+        # GPS sensors - use can, ais, or both? which takes priority? true heading / track made good usage?
         self.gps_lat = self.getTrustedAndAvg(self.past_gps_lat,
-                                             [data.gps_0_latitude, data.gps_1_latitude])
+                                             [data.gps_can_latitude_degreeMinutes,
+                                              data.gps_ais_latitude_degreeMinutes])
         self.gps_lon = self.getTrustedAndAvg(self.past_gps_lon,
-                                             [data.gps_0_longitude, data.gps_1_longitude])
+                                             [data.gps_can_longitude_degreeMinutes,
+                                              data.gps_ais_longitude_degreeMinutes])
         self.gps_headingDegrees = self.getTrustedAndAvg(self.past_gps_headingDegrees,
-                                                        [self.convertDegrees(data.gps_0_true_heading),
-                                                         self.convertDegrees(data.gps_1_true_heading)])
+                                                        [self.convertDegrees(data.gps_can_true_heading_degrees),
+                                                         self.convertDegrees(data.gps_ais_true_heading_degrees)])
         self.gps_speedKmph = self.getTrustedAndAvg(self.past_gps_speedKmph,
-                                                   [data.gps_0_groundspeed * KNOTS_TO_KMPH,
-                                                    data.gps_1_groundspeed * KNOTS_TO_KMPH])
+                                                   [data.gps_can_groundspeed_knots * KNOTS_TO_KMPH,
+                                                    data.gps_ais_groundspeed_knots * KNOTS_TO_KMPH])
         # Wind sensors
         self.wind_speedKmph = self.getTrustedAndAvg(self.past_wind_speedKmph,
-                                                    [data.wind_sensor_0_speed * KNOTS_TO_KMPH,
-                                                     data.wind_sensor_1_speed * KNOTS_TO_KMPH,
-                                                     data.wind_sensor_2_speed * KNOTS_TO_KMPH])
+                                                    [data.wind_sensor_1_speed_knots * KNOTS_TO_KMPH,
+                                                     data.wind_sensor_2_speed_knots * KNOTS_TO_KMPH,
+                                                     data.wind_sensor_3_speed_knots * KNOTS_TO_KMPH])
         self.wind_direction = self.getTrustedAndAvg(self.past_wind_direction,
-                                                    [self.convertDegrees(data.wind_sensor_0_direction),
-                                                     self.convertDegrees(data.wind_sensor_1_direction),
-                                                     self.convertDegrees(data.wind_sensor_2_direction)])
+                                                    [self.convertDegrees(data.wind_sensor_1_angle_degrees),
+                                                     self.convertDegrees(data.wind_sensor_2_angle_degrees),
+                                                     self.convertDegrees(data.wind_sensor_3_angle_degrees)])
 
     # (0 is up, 90 is right, etc - cw - degrees) -> (0 is right, 90 is up, etc - ccw - degrees)
     def convertDegrees(self, degree):
