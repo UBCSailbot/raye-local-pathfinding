46f0379eb7504816557e019fd7f0546606abfda3
Tyler Lum <tylergwlum@gmail.com>
2021-07-05 11:55:42 -0700

    Add parameter sweep bash script

--- parameter_sweep.bash
+++ parameter_sweep.bash
@@ -13,12 +13,12 @@ for planner_type in "rrtstar" "prmstar" "rrtsharp" "rrtxstatic"
 do
 for global_wind_direction_degrees in 0 30 45 90
 do
-for num_ais_ships in 0 5 20 50
+for num_ais_ships in 5 20 50
 do
-for random_seed in "1" "12"
+for random_seed in "1" "310"
 do
-echo "roslaunch local_pathfinding path_evaluator.launch num_runs:=$num_runs runtime_seconds:=$runtime_seconds state_sampler:=$state_sampler grid_n:=$grid_n planner_type:=$planner_type global_wind_direction_degrees:=$global_wind_direction_degrees num_ais_ships:=$num_ais_ships random_seed:=$random_seed"
-timeout 120 roslaunch local_pathfinding path_evaluator.launch num_runs:=$num_runs runtime_seconds:=$runtime_seconds state_sampler:=$state_sampler grid_n:=$grid_n planner_type:=$planner_type global_wind_direction_degrees:=$global_wind_direction_degrees num_ais_ships:=$num_ais_ships random_seed:=$random_seed || :
+echo "timeout $(echo "scale=4; $num_runs*$runtime_seconds+30" | bc) roslaunch local_pathfinding path_evaluator.launch num_runs:=$num_runs runtime_seconds:=$runtime_seconds state_sampler:=$state_sampler grid_n:=$grid_n planner_type:=$planner_type global_wind_direction_degrees:=$global_wind_direction_degrees num_ais_ships:=$num_ais_ships random_seed:=$random_seed"
+timeout $(echo "scale=4; $num_runs*$runtime_seconds+30" | bc) roslaunch local_pathfinding path_evaluator.launch num_runs:=$num_runs runtime_seconds:=$runtime_seconds state_sampler:=$state_sampler grid_n:=$grid_n planner_type:=$planner_type global_wind_direction_degrees:=$global_wind_direction_degrees num_ais_ships:=$num_ais_ships random_seed:=$random_seed || :
 done
 done
 done
