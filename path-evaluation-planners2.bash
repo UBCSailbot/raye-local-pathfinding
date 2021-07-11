cf6b47055f926e3817e3eb32f34d86268ea2030d
patrick-5546 <pcreighton429@gmail.com>
2021-07-11 14:17:26 -0700

    Improve and rename wandb bash scripts

--- path-evaluation-planners2.bash
+++ path-evaluation-planners2.bash
@@ -5,8 +5,11 @@ for total_runtime in 1.0 2.0 4.0
 do
     for runtime_seconds in 0.5 1.0 2.0 4.0
     do
+        if (( $(echo "$runtime_seconds > $total_runtime" |bc -l) ))
+        then
+            continue
+        fi
         num_runs=$(echo "$total_runtime / $runtime_seconds" | bc)
-
         for grid_n in 10 15
         do
             for planner_type in "prmstar" "lazyprmstar" "rrtxstatic" "rrtsharp"
@@ -17,11 +20,6 @@ do
                     do
                         for random_seed in "1" "310"
                         do
-                        if (( $(echo "$runtime_seconds > $total_runtime" |bc -l) ))
-                        then
-                            continue
-                        fi
-
                         echo "timeout $(echo "scale=4; $num_runs*$runtime_seconds+30" | bc) roslaunch local_pathfinding path_evaluator.launch num_runs:=$num_runs runtime_seconds:=$runtime_seconds state_sampler:="grid" grid_n:=$grid_n planner_type:=$planner_type global_wind_direction_degrees:=$global_wind_direction_degrees num_ais_ships:=$num_ais_ships random_seed:=$random_seed"
 
                         timeout $(echo "scale=4; $num_runs*$runtime_seconds+30" | bc) roslaunch local_pathfinding path_evaluator.launch num_runs:=$num_runs runtime_seconds:=$runtime_seconds state_sampler:="grid" grid_n:=$grid_n planner_type:=$planner_type global_wind_direction_degrees:=$global_wind_direction_degrees num_ais_ships:=$num_ais_ships random_seed:=$random_seed || :
