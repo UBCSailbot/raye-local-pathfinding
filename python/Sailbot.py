835c7339fec0eca9d433238ca0b69da177fa3f23
Jamen Kaye <jamen.kaye@gmail.com>
2022-08-24 00:11:48 -0700

    handled invalid AIS coordinates

--- python/Sailbot.py
+++ python/Sailbot.py
@@ -176,6 +176,13 @@ class Sailbot:
         self.globalWindLastUpdate = time.time()
 
     def AISCallback(self, data):
+
+        for ship in data.ships:
+            if not utils.checkLatLonIsValid(latlon(ship.lat, ship.lon)):
+                data.ships.remove(ship)
+                rospy.logwarn_throttle(5, "Invalid co-ordinates for "
+                                       + "AIS Ship ID {}: ({}, {})".format(ship.ID, ship.lat, ship.lon))
+
         self.AISData = data
         self.AISLastUpdate = time.time()
 
